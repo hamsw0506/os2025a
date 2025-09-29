c9b90e3e7a3460efcf42ca8ce653f7de8af2954c
hamsw0506 <hamsw0506@gmail.com>
2025-09-29 12:00:52 +0900

    v0.3 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,16 +6,6 @@ import (
 )
 
 func main() {
-	// var name = "Ham Sunwoo"
-	// fmt.Println(name)
-	// fmt.Println(math.Ceil(2.71))
-	// fmt.Println(strings.Title("head first go"))
-
-	// var name string
-	// name = "Ham Sunwoo"
-	// fmt.Println(name)
-
-	var name float64
-	name = 2.71
+	var name bool
 	fmt.Println(name, reflect.TypeOf(name))
 }
