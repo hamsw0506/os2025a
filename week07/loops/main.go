839e663ecffda734adf12a8081e806ac095d5e04
hamsw0506 <hamsw0506@gmail.com>
2025-10-13 10:48:33 +0900

    loops/main.go v0.1 time & strings package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strings"
+	"time"
 )
 
 func main() {
-	var length float64 = 3.2
-	var width int = 2
-	fmt.Println("Area is", int(length)*width)
-	fmt.Println("length > width?", int(length) > width)
-	fmt.Println("con", reflect.TypeOf(int(length)))
-	fmt.Println("origin", reflect.TypeOf(length))
+	var now time.Time = time.Now()
+	var day int = now.Day()
+	fmt.Println(day)
+	univ := "Go Inha$"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
