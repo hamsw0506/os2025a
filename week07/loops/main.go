3fcee13bf5f06e41ec1fa031926dfedba8a9c013
hamsw0506 <hamsw0506@gmail.com>
2025-11-03 10:32:41 +0900

    week07/loops v??

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,37 +1,48 @@
+// guess challenges players to guess a random number.
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	//var fmt string = "inha"
-	// var int int = 7
-	// fmt.Println(int)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?", target)
 
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n')
-	//fmt.Println(err)
-	if err != nil {
-		log.Fatal(err) //repot the error and exit program
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
 
-	i = strings.TrimSpace(i)
-	score, err := strconv.ParseFloat(i, 64)
-	if err != nil {
-		log.Fatal(err)
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
 	}
-
-	var pf string
-	if score >= 60 {
-		pf = "Pass"
-	} else {
-		pf = "Fail"
-	}
-	fmt.Println(pf, score)
 }
