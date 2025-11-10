07bd0307e4bfd54ec572ec07d7df819ad89634c3
hamsw0506 <hamsw0506@gmail.com>
2025-11-10 12:14:26 +0900

    week11 slice v0.7

---
+++ week11/cmd/ch06-ex03/main.go
@@ -0,0 +1,48 @@
+// Package datafile allows reading data samples from files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// GetFloats reads a float64 from each line of a file.
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return numbers, nil
+}
+
+func main() {
+	weights, err := GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hap := 0.0
+	for i := 0; i < len(weights); i++ {
+		hap = hap + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("주차별 고기 소비량 평균: ", hap/weeks)
+}
