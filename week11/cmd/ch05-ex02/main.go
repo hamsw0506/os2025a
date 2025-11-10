75344da7af5639551d7a55c4c5a2f92ee082e395
hamsw0506 <hamsw0506@gmail.com>
2025-11-10 10:54:17 +0900

    week11 array v0.7

---
+++ week11/cmd/ch05-ex02/main.go
@@ -0,0 +1,23 @@
+// average calculates the average of several numbers.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func main() {
+	weights, err := datafile.GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sum := 0.0
+	//for _, number := range numbers {
+	for i := 0; i < len(weights); i++ {
+		sum = sum + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("주차별 고기 소비량 평균 : ", sum/weeks)
+}
