bc540c06bc42ed3357a0e5f73a6329df3e371618
hamsw0506 <hamsw0506@gmail.com>
2025-12-01 12:16:11 +0900

    week14 goroutine v0.4

--- week14/cmd/ch13-ex02/main.go
+++ week14/cmd/ch13-ex02/main.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
+func abc(channel chan string) {
+	channel <- "내고향 스페셜\n"
+	channel <- "KBS 뉴스광장\n"
+	channel <- "인간극장\n"
+}
+
+func def(channel chan string) {
+	channel <- "건강의 재구성 설록(재)\n"
+	channel <- "오늘N\n"
+	channel <- "찾아가는 꾸러기 교실\n"
+}
+
 func main() {
-	ch := make(chan int)
-
-	go func() {
-		ch <- 123
-	}()
-
-	x := <-ch
-	fmt.Println(x)
+	kbs := make(chan string)
+	mbc := make(chan string)
+	go abc(kbs)
+	go def(mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Println()
 }
